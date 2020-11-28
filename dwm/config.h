892eb98981b540a435dd3c06fd90d85d89c57650
Daniel Bartsch <daniel.bartsch@live.at>
2020-11-28 22:32:47 +0100

    added new screenshotting utility, as spectacle wasn't starting

--- dwm/config.h
+++ dwm/config.h
@@ -91,7 +91,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } },
 	{ 0,                   XF86XK_AudioRaiseVolume,	spawn,		    SHCMD("pamixer --allow-boost -i 1; xsetroot -name \"`getStatusBar`\"") },
 	{ 0,                   XF86XK_AudioLowerVolume,	spawn,		    SHCMD("pamixer --allow-boost -d 1; xsetroot -name \"`getStatusBar`\"") },
-	{ 0,                   XK_Print,	            spawn,		    SHCMD("spectacle") },
+	{ 0,                   XK_Print,	            spawn,		    SHCMD("screenshot_clip") },
 	TAGKEYS(               XK_q,                    1)
 	TAGKEYS(               XK_d,                    2)
 	TAGKEYS(               XK_r,                    3)
