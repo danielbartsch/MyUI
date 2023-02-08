886ea8115c14e632e3c3e4898b454b4d195e7ddb
Daniel Bartsch <daniel.bartsch@live.at>
2023-02-08 23:19:13 +0100

    format

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon		Command					Update Interval		Update Signal*/
-	{"",        "echo \"\"", 0, 0},
-	{"",		"sb_network_traffic",			1,			0},
-	{"",		"sb_connection_status",			300,			0},
-	{"",		"sb_updates",				1800,			0},
-	{"",		"sb_controller_battery",		60,			0},
-	{"",		"sb_ram_usage",				60,			0},
-	{"",		"sb_weather",				300,			0},
-	{"",		"sb_temperature",			60,			0},
-	{"",		"sb_volume",				0,			2},
-	{"",		"sb_datum",				60,			0},
-	{"",		"sb_up_time",				60,			0},
+	{"",        "echo \"\"", 			0,					0},
+	{"",		"sb_network_traffic",	1,					0},
+	{"",		"sb_connection_status",	300,				0},
+	{"",		"sb_updates",			1800,				0},
+	{"",		"sb_controller_battery",60,					0},
+	{"",		"sb_ram_usage",			60,					0},
+	{"",		"sb_weather",			300,				0},
+	{"",		"sb_temperature",		60,					0},
+	{"",		"sb_volume",			0,					2},
+	{"",		"sb_datum",				60,					0},
+	{"",		"sb_up_time",			60,					0},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
